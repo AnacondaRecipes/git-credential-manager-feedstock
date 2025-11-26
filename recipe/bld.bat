ac04ee3ee431a503b159b7d08d83bd37f8c73f99
vchychy <vchychyrko@anaconda.com>
2025-11-26 13:56:37 +0200

    init

--- recipe/bld.bat
+++ recipe/bld.bat
@@ -1,14 +1,14 @@
 cd src\windows\Installer.Windows\
 if errorlevel 1 exit /b %errorlevel%
 
-powershell -Command ".\layout.ps1 -Configuration WindowsRelease -Output payload -SymbolOutput symbols"
+powershell -NoLogo -NoProfile -NonInteractive -ExecutionPolicy Bypass -File ".\layout.ps1" -Configuration WindowsRelease -Output payload -SymbolOutput symbols
 if errorlevel 1 exit /b %errorlevel%
 
 cd payload
 if errorlevel 1 exit /b %errorlevel%
 
 rem Already exists in the package
-del NOTICE
+if exist NOTICE del NOTICE
 if errorlevel 1 exit /b %errorlevel%
 
 move * %LIBRARY_BIN%
